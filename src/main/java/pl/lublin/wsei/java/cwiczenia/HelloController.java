f1c0f0b257b258b7be647880c0c3f54ea78de468
SylwesterPudelski92 <sylwesterpudelski1992@gmail.com>
2022-04-25 10:20:05 +0200

    kolejne zmiany, m.in. wyswietlenie grafiki, możliwość wybierania konkretnych grafik z listy (addListerner)

--- src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
@@ -1,12 +1,18 @@
 package pl.lublin.wsei.java.cwiczenia;
 
 import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -15,26 +21,45 @@ public class HelloController {
 
     public Label lbFile;
     public ListView lstInfografiki;
+    public ImageView imgMiniaturka;
+    public Button btnPokazInfografike;
+    public TextField txtAdresStrony;
+    public Button btnPrzejdzDoStrony;
     ObservableList<String> tytuly = FXCollections.observableArrayList();
     GusInfoGraphicList igList;
 
 
     FileChooser fileChooser = new FileChooser();
     FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)", "*.xml");
+
     @FXML
-    public void initialize(){
+    public void initialize() {
         fileChooser.getExtensionFilters().add(xmlFilter);
+        lstInfografiki.getSelectionModel().selectedIndexProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observableValue, Number old_val, Number new_val) {
+                        int index = new_val.intValue();
+                        if (index != -1) {
+                            txtAdresStrony.setText(igList.infografiki.get(index).adresStrony);
+                            Image image = new Image(igList.infografiki.get(index).adresMiniaturki);
+                            imgMiniaturka.setImage(image);
+                        } else {
+                            txtAdresStrony.setText("");
+                            imgMiniaturka.setImage(null);
+                        }
+                    }
+                });
     }
 
     public void btnOpenFileAction(ActionEvent actionEvent) {
         File file = fileChooser.showOpenDialog(null);
-        if (file != null){
+        if (file != null) {
             igList = new GusInfoGraphicList(file.getAbsolutePath());
             lbFile.setText(file.getAbsolutePath());
-            for (Infografika ig: igList.infografiki)tytuly.add(ig.tytul);
+            for (Infografika ig : igList.infografiki) tytuly.add(ig.tytul);
             lstInfografiki.setItems(tytuly);
-        }
-        else{
+        } else {
             lbFile.setText("Proszę wczytać plik ...");
         }
     }
