056e1308522630ae66081f7b31c8408e954bc047
SylwesterPudelski92 <sylwesterpudelski1992@gmail.com>
2022-04-25 09:45:16 +0200

    dodanie listy Infografik

--- src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
@@ -1,8 +1,12 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -10,6 +14,11 @@ import java.io.File;
 public class HelloController {
 
     public Label lbFile;
+    public ListView lstInfografiki;
+    ObservableList<String> tytuly = FXCollections.observableArrayList();
+    GusInfoGraphicList igList;
+
+
     FileChooser fileChooser = new FileChooser();
     FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)", "*.xml");
     @FXML
@@ -20,7 +29,10 @@ public class HelloController {
     public void btnOpenFileAction(ActionEvent actionEvent) {
         File file = fileChooser.showOpenDialog(null);
         if (file != null){
+            igList = new GusInfoGraphicList(file.getAbsolutePath());
             lbFile.setText(file.getAbsolutePath());
+            for (Infografika ig: igList.infografiki)tytuly.add(ig.tytul);
+            lstInfografiki.setItems(tytuly);
         }
         else{
             lbFile.setText("Proszę wczytać plik ...");
